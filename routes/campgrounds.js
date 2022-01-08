930f300723f2637f2f4d2ee2d1c8efc326151fad
Michelangelo <michelangelocalori@gmail.com>
2022-01-08 19:32:40 +0000

    removed logs

--- routes/campgrounds.js
+++ routes/campgrounds.js
@@ -12,7 +12,6 @@ const validateCampground = (req, res, next) =>
     const { error } = campgroundSchema.validate(req.body);
     if (error)
     {
-        console.log(error)
         const msg = error.details.map(el => el.message).join(',')
         throw new ExpressError(msg, 400)
     } else
